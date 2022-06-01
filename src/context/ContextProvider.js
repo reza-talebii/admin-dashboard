debed03d9f5bf7ac351d311f39ef3eba7087f8ce
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-06-01 11:26:15 +0430

    complete provider

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -2,7 +2,13 @@ import React, { createContext } from "react";
 
 const StateContext = createContext();
 
-
 const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
 
-}
+export const ContextProvider = ({ children }) => {
+  return <StateContext.Provider value={{}}>{children}</StateContext.Provider>;
+};
