bd98f8974768ffe3c8551082f5c1a0098d6befd4
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-06-01 11:22:33 +0430

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,7 +20,7 @@ const Sidebar = () => {
     <aside className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
         <>
-          <div className="flex justify-between items-center">
+          <section className="flex justify-between items-center">
             <Link
               to="/"
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
@@ -32,13 +32,14 @@ const Sidebar = () => {
                 type="button"
                 onClick={() => setActiveMenu(!activeMenu)}
                 style={{ color: "black" }}
-                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block "
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
               </button>
             </TooltipComponent>
-          </div>
-          <div className="mt-10 ">
+          </section>
+
+          <section className="mt-10 ">
             {links.map((item) => (
               <div key={item.title}>
                 <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
@@ -61,7 +62,7 @@ const Sidebar = () => {
                 ))}
               </div>
             ))}
-          </div>
+          </section>
         </>
       )}
     </aside>
