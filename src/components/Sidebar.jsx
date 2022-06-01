69a9f30c3acd7f4dfad96d428bc454a42f2c9c4c
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-06-01 11:33:31 +0430

    complete close sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,9 +7,10 @@ import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { links } from "../data/dummy";
+import { useStateContext } from "../context/ContextProvider";
 
 const Sidebar = () => {
-  const activeMenu = true;
+  const { setActiveMenu, activeMenu } = useStateContext();
 
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
@@ -23,6 +24,7 @@ const Sidebar = () => {
           <section className="flex justify-between items-center">
             <Link
               to="/"
+              onClick={() => setActiveMenu(false)}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware /> <span>Shoppy</span>
@@ -32,7 +34,7 @@ const Sidebar = () => {
                 type="button"
                 onClick={() => setActiveMenu(!activeMenu)}
                 style={{ color: "black" }}
-                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block "
               >
                 <MdOutlineCancel />
               </button>
