aefb0a703e48b2da49ce495bc8ed16ad5bd22d74
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-06-02 16:02:07 +0430

    complete carts

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -17,7 +17,7 @@ const Ecommerce = () => {
   return (
     <section className="mt-12">
       <div className="flex flex-wrap lg:flex-nowrap justify-center ">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+        <section className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
@@ -33,7 +33,31 @@ const Ecommerce = () => {
               size="md"
             />
           </div>
-        </div>
+        </section>
+
+        <section className="flex m-3 flex-wrap justify-center gap-1 items-center">
+          {earningData.map((i) => (
+            <div
+              className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
+              key={i.title}
+            >
+              <button
+                type="button"
+                style={{ color: i.iconColor, background: i.iconBg }}
+                className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+              >
+                {i.icon}
+              </button>
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{i.amount}</span>
+                <span className={`text-sm text-${i.pcColor} m-2`}>
+                  {i.percentage}
+                </span>
+              </p>
+              <p className="text-sm text-gray-400 mt-1">{i.title}</p>
+            </div>
+          ))}
+        </section>
       </div>
     </section>
   );
