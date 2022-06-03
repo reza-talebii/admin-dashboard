80d66d3c53db9a47ad104188c27129d05b50a8ea
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-06-03 17:06:45 +0430

    complete calendar app

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import {
   ScheduleComponent,
-  ViewsDirective,
-  ViewDirective,
   Day,
   Week,
   WorkWeek,
@@ -18,7 +16,21 @@ import { scheduleData } from "../data/dummy";
 import { Header } from "../components";
 
 const Calendar = () => {
-  return <div>Calendar</div>;
+  return (
+    <section className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category={"App"} title="Calendar" />
+
+      <ScheduleComponent
+        height={"650px"}
+        eventSettings={{ dataSource: scheduleData }}
+        selectedDate={new Date(2021, 0, 10)}
+      >
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
+    </section>
+  );
 };
 
 export default Calendar;
