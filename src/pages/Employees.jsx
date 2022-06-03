dc08b04b51b9c5e0e6b9a5315a89e3e1479b64c7
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-06-03 16:32:29 +0430

    complete Exployese page

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Search,
+  Page,
+  Inject,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
+
+import { employeesData, employeesGrid } from "../data/dummy";
+
+import { Header } from "../components";
+
 const Employees = () => {
-  return <div>Employees</div>;
+  return (
+    <section className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="employees" category="page" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={["search"]}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => {
+            <ColumnDirective key={index} {...item} />;
+          })}
+        </ColumnsDirective>
+        <Inject services={[Search, Page, Toolbar]} />
+      </GridComponent>
+    </section>
+  );
 };
 
 export default Employees;
