5d28f325e8e7dff7403239a46dac85d598dbe943
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-06-03 16:17:58 +0430

    complete header comp

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+import {
+  GridComponent,
+  ColumnsDirective,
+  // CoumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
+
+import { Header } from "../components";
+
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <section>
+      <Header title="Orders" category="page" />
+    </section>
+  );
 };
 
 export default Orders;
